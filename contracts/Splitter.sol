d65e34ab5a83d0ca3194eed8f1aaa6b4b8036e3f
Atik <atiklabs@pm.me>
2019-04-29 16:07:54 +0200

    Checking msg.value and adding message to requires.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -12,9 +12,9 @@ contract Splitter {
     event LogEtherWithdraw2(uint amount);
 
     constructor(address _beneficiary1, address _beneficiary2) public {
-        require(_beneficiary1 != _beneficiary2);
-        require(_beneficiary1 != address(0));  // Check malformed addresses
-        require(_beneficiary2 != address(0));
+        require(_beneficiary1 != _beneficiary2, "Beneficiaries must be different");
+        require(_beneficiary1 != address(0), "Beneficiary1 address is malformed");
+        require(_beneficiary2 != address(0), "Beneficiary2 address is malformed");
         // I decided that is the creator of the contract who is the payer (it looks convenient)
         payer = msg.sender;
         // beneficiaries
@@ -27,7 +27,8 @@ contract Splitter {
     // Alice sends ether to the contract with pay(), for it to be split,
     // half of it goes to Bob and the other half to Carol.
     function pay() external payable {
-        require(msg.sender == payer);
+        require(msg.sender == payer, "Only payer can execute this function");
+        require(msg.value > 0, "This function asks for some value to be sent.");
         uint _addedAmount = msg.value/uint(2);
         toWithdraw1 += _addedAmount;
         toWithdraw2 += _addedAmount;
@@ -36,15 +37,15 @@ contract Splitter {
 
     // Beneficiaries can withdraw, all at once for the sake of simplicity
     function withdraw() public {
-        require(msg.sender == beneficiary1 || msg.sender == beneficiary2);
+        require(msg.sender == beneficiary1 || msg.sender == beneficiary2, "Only beneficiaries can withdraw");
         if (msg.sender == beneficiary1) {
-            require(toWithdraw1 > 0);
+            require(toWithdraw1 > 0, "Nothing to withdraw");
             msg.sender.transfer(toWithdraw1);
             emit LogEtherWithdraw1(toWithdraw1);
             toWithdraw1 = 0;
         }
         if (msg.sender == beneficiary2) {
-            require(toWithdraw2 > 0);
+            require(toWithdraw2 > 0, "Nothing to withdraw");
             msg.sender.transfer(toWithdraw2);
             emit LogEtherWithdraw2(toWithdraw2);
             toWithdraw2 = 0;
